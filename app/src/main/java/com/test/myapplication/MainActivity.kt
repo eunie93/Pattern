c2edc79257261ddcb037ff11c0f1c425b1a98c65
eunie <eunie@bankq.co.kr>
2020-12-02 14:18:29 +0900

    shallow copy deep copy

--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.test.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         logManager.print("출력")
+
+        // Shallow Copy
+        // 단순 대입이라 다른 리스트 값에 영향을 미침
+        var copy1 = ArrayList<String>()
+        copy1.add("A")
+        copy1.add("B")
+        var copy2 = ArrayList<String>()
+        copy2 = copy1
+        copy2.removeAt(1)
+
+        // Deep Copy
+        // 다른 리스트로부터 아이템만 복사해와서 값을 삭제해도 영향을 미치지 않음
+        var copy3 = ArrayList<String>()
+        copy3.add("A")
+        copy3.add("B")
+        var copy4 = ArrayList<String>()
+        copy4.addAll(copy3)
+        copy4.removeAt(1)
+
     }
 }
