2980c9d8012e13e4d68eb23d77c3f9f71aeee9f3
eunie <eunie@bankq.co.kr>
2020-12-02 15:36:01 +0900

    재귀

--- app/src/main/java/com/test/myapplication/logManager.kt
+++ app/src/main/java/com/test/myapplication/logManager.kt
@@ -2,10 +2,11 @@ package com.test.myapplication
 
 import javax.inject.Singleton
 
+
 @Singleton
 class logManager {
     companion object {
-        fun print(msg: String) {
+        fun printMsg(msg: String) {
             print(msg)
         }
     }
