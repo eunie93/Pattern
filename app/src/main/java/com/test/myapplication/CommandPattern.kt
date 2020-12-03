f623c5e9ebb5c1608287f8b1fb2d3b8681fbde55
eunie <eunie@bankq.co.kr>
2020-12-03 12:04:53 +0900

    command Pattern

---
+++ app/src/main/java/com/test/myapplication/CommandPattern.kt
@@ -0,0 +1,5 @@
+package com.test.myapplication
+
+interface CommandPattern {
+    fun run()
+}
--- app/src/main/java/com/test/myapplication/MainActivity.kt
+++ app/src/main/java/com/test/myapplication/MainActivity.kt
@@ -2,47 +2,121 @@ package com.test.myapplication
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
-    var pattern: StatePattern? = null
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val join = Join()
-        val main = Main()
-        val err = Err()
-        val login = Login()
+        /*  val join = Join()
+          val main = Main()
+          val err = Err()
+          val login = Login()
+          var randomNum = Random()
+          val num = randomNum.nextInt(4)
+          var random = arrayOf("로그인", "로그아웃", "비회원", "세션만료", "서버응답없음")
+          var state = random[num]
+
+          Log.d("상태체크", "$num $state")
+
+          when (state) {
+              "로그인" -> {
+                  pattern = main
+                  pattern!!.showState()
+              }
+              "로그아웃", "세선만료" -> {
+                  pattern = login
+                  pattern!!.showState()
+              }
+              "비회원" -> {
+                  pattern = join
+                  pattern!!.showState()
+              }
+              "서버응답없음" -> {
+                  pattern = err
+                  pattern!!.showState()
+              }
+          }*/
         var randomNum = Random()
-        val num = randomNum.nextInt(4)
-        var random = arrayOf("로그인", "로그아웃", "비회원", "세션만료", "서버응답없음")
+        var random = arrayOf("인증", "가입", "푸시")
+        val num = randomNum.nextInt(random.size-1)
         var state = random[num]
 
         Log.d("상태체크", "$num $state")
+        button.text = state
+        button.setOnClickListener(this)
+    }
 
-        when (state) {
-            "로그인" -> {
-                pattern = main
-                pattern!!.showState()
-            }
-            "로그아웃", "세선만료" -> {
-                pattern = login
-                pattern!!.showState()
-            }
-            "비회원" -> {
-                pattern = join
-                pattern!!.showState()
-            }
-            "서버응답없음" -> {
-                pattern = err
-                pattern!!.showState()
+    override fun onClick(p0: View?) {
+        var command = commandFunc()
+        var login = Login()
+        var join = Join()
+        var push = Push()
+
+        when(p0){
+            button->{
+                when(button.text){
+                    "인증"->{
+                        command.setPattern(login)
+                        command.run()
+                    }
+                    "가입"->{
+                        command.setPattern(join)
+                        command.run()
+                    }
+                    "푸시"->{
+                        command.setPattern(push)
+                        command.run()
+                    }
+                }
             }
         }
     }
 
-    class Main: StatePattern {
+    class commandFunc {
+        var command: CommandPattern? = null
+
+        fun setPattern(command: CommandPattern) {
+            this.command = command
+        }
+        fun run() {
+            command!!.run()
+        }
+    }
+
+
+    class Login : CommandPattern {
+        fun loginFun() {
+            Log.d("체크해보자", "로그인")
+        }
+        override fun run() {
+            loginFun()
+        }
+    }
+
+    class Join : CommandPattern {
+        fun joinFun() {
+            Log.d("체크해보자", "가입")
+        }
+        override fun run() {
+            joinFun()
+        }
+    }
+
+    class Push : CommandPattern {
+        fun pushFun() {
+            Log.d("체크해보자", "푸시")
+        }
+        override fun run() {
+            pushFun()
+        }
+    }
+
+  /*  class Main : StatePattern {
         override fun showState() {
             Log.d("상태체크", "Main 고고")
         }
@@ -64,5 +138,5 @@ class MainActivity : AppCompatActivity() {
         override fun showState() {
             Log.d("상태체크", "에러화면 고고고")
         }
-    }
+    }*/
 }
